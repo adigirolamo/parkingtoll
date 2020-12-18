2391770faa7859b52798798e11a759984814de74
adigirolamo <digirolamo18@gmail.com>
2020-12-18 01:16:27 +0100

    project start

---
+++ src/main/java/com/adigi/parkingtoll/ParkingtollApplication.java
@@ -0,0 +1,13 @@
+package com.adigi.parkingtoll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingtollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingtollApplication.class, args);
+	}
+
+}
